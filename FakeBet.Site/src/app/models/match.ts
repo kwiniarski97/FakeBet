1fe9ad02bbd9309fc2d7b5d348297b540dfc9783
Unknown <kwiniarski97@gmail.com>
2018-03-09 12:26:47 +0100

    added flag support

--- FakeBet.Site/src/app/models/match.ts
+++ FakeBet.Site/src/app/models/match.ts
@@ -5,8 +5,10 @@ export class Match {
   matchStatus: MatchStatus;
   teamAName: string;
   teamAPoints: number;
+  teamANationalityCode: string;
   teamBName: string;
   teamBPoints: number;
+  teamBNationalityCode: string;
   pointsRatio: number;
   votes: any[]; // todo change to some vote interface or model
 }
